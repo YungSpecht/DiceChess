8642d9c533c20ab34818257ad78acbb4019e7a4e
YungSpecht <burneraee@gmail.com>
2022-09-16 11:09:16 +0200

    blank libgdx project

---
+++ desktop/src/com/group4/dicechess/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.group4.dicechess;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.group4.dicechess.DiceChess;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("DiceChess");
+		new Lwjgl3Application(new DiceChess(), config);
+	}
+}
