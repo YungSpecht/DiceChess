d130dd85285916ac407b0f053ca57916e80d04fd
Rango <39035567+YungSpecht@users.noreply.github.com>
2023-01-23 15:56:03 +0100

    reverting to old expectimax

--- core/src/com/group4/dicechess/agents/Expectimax/ExpectimaxTree.java
+++ core/src/com/group4/dicechess/agents/Expectimax/ExpectimaxTree.java
@@ -1,8 +1,6 @@
 package com.group4.dicechess.agents.Expectimax;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 import com.group4.dicechess.GameState;
 import com.group4.dicechess.Representation.Move;
@@ -13,163 +11,65 @@ public class ExpectimaxTree {
     private final GameState state;
     private final ArrayList<Move> possibleOutcomes;
 
+    public static void main(String[] args) {
+        GameState test = new GameState();
+        test.diceRoll();
+        ExpectimaxTree tree = new ExpectimaxTree(test);
+        tree.buildTree(12);
+        System.out.println(tree.getBestMove().getPiece().getId());
+    }
 
     public Node getRoot(){
         return root;
     }
 
     public ExpectimaxTree(GameState currentState){
-        //new NN_Evaluation();
         root = new Node(currentState);
         state = currentState;
         possibleOutcomes = state.getPossibleMoves();
+        new NN_Evaluation();
     }
 
     public void buildTree(int depth){
-        starMinMax(root, state, depth, Double.MIN_VALUE, Double.MAX_VALUE);
+        buildLevel(root, state, depth);
     }
 
-    private void starMinMax(Node pointer, GameState game,  int depth, double alpha, double beta){
-        if(depth <= 0) {
-            pointer.setValue(game.evaluate());
-        }
-        else if(pointer.isChanceNode()){
-            boolean cutoff = false;
-            ArrayList<Integer> rolls = game.getRollsList();
-            ArrayList<Integer> r = new ArrayList<Integer>(rolls);
-            double u = 19455;
-            double A = r.size() * (alpha - u) + u;
-            double l = -19455;
-            double B = r.size() * (beta - l) + l;
-            double vsum = 0;
-            for(int i = 0; i < r.size(); i++){
-                double AX = Math.max(A, l);
-                double BX = Math.min(B, u);
-                game.setDiceRoll(r.get(i));
-                Node childNode = new Node(game);
-                childNode.setParent(pointer);
-                pointer.addChild(childNode);
-                starMinMax(childNode, game,depth-1, AX, BX);
-                vsum += childNode.getValue();
-                if(childNode.getValue() <= A){
-                    vsum += u * (r.size() - (i + 1));
-                    pointer.setValue(vsum/r.size());
-                    cutoff = true;
-                    break;
+    private void buildLevel(Node pointer, GameState game,  int depth){
+
+        if(pointer.isChanceNode()){
+            if(depth > 0 ){
+                ArrayList<Integer> rolls = game.getRollsList();
+                ArrayList<Integer> r = new ArrayList<Integer>(rolls);
+                for(int roll : r){
+                    game.setDiceRoll(roll);
+                    Node childNode = new Node(game);
+                    childNode.setParent(pointer);
+                    pointer.addChild(childNode);
+                    buildLevel(childNode, game, depth-1);
                 }
-                if(childNode.getValue() >= B){
-                    vsum += l * (r.size() - (i + 1));
-                    pointer.setValue(vsum/r.size());
-                    cutoff = true;
-                    break;
-                }
-                A += u - childNode.getValue();
-                B += l - childNode.getValue();
+                game.setDiceRoll(0);
             }
-            game.setDiceRoll(0);
-            if(pointer.getChildren().size() == 0){
-                pointer.setValue(game.evaluate());
-            }
-            else if(!cutoff){
-                pointer.setValue(vsum/rolls.size());
-            }
+            pointer.computeValue(game);
         }
         else{
-            ArrayList<ArrayList<Move>> mL = game.getMoveList();
-            ArrayList<Move> possibleMoves = game.getPossibleMoves();
-            ArrayList<Move> pM = new ArrayList<Move>(possibleMoves);
-            //sortMoveList(pM, game);
-            double value;
-            if(game.turnCounter % 2 == 0){
-                value = Double.MIN_VALUE;
+            if(depth > 0){
+                ArrayList<Move> possibleMoves = game.getPossibleMoves();
+                ArrayList<Move> pM = new ArrayList<Move>(possibleMoves);
                 for(Move m : pM){
                     game.movePiece(m.getStart().getRow(), m.getStart().getCol(), m.getDestination().getRow(), m.getDestination().getCol(), true);
                     Node childNode = new Node(game);
                     childNode.setParent(pointer);
                     pointer.addChild(childNode);
-                    starMinMax(childNode, game, depth-1, alpha, beta);
+                    buildLevel(childNode, game, depth-1);
                     game.reverseLastMove();
-                    game.setMoveList(mL);
-                    value = Math.max(value, childNode.getValue());
-                    if(value > beta){
-                        break;
-                    }
-                    alpha = Math.max(alpha, value);
                 }
             }
-            else{
-                value = Double.MAX_VALUE;
-                for(Move m : pM){
-                    game.movePiece(m.getStart().getRow(), m.getStart().getCol(), m.getDestination().getRow(), m.getDestination().getCol(), true);
-                    Node childNode = new Node(game);
-                    childNode.setParent(pointer);
-                    pointer.addChild(childNode);
-                    starMinMax(childNode, game, depth-1, alpha, beta);
-                    game.reverseLastMove();
-                    game.setMoveList(mL);
-                    value = Math.min(value, childNode.getValue());
-                    if(value < alpha){
-                        break;
-                    }
-                    beta = Math.min(beta, value);
-                }
-            }
-            if(pointer.getChildren().size() == 0){
-                pointer.setValue(game.evaluate());
-            }
-            else{
-                pointer.setValue(value);
-            }
+            pointer.computeValue(game);
         }
-    }
-
-    private void sortMoveList(ArrayList<Move> moves, GameState game){
-        ArrayList<Double> values = new ArrayList<Double>();
-        ArrayList<Move> result = new ArrayList<Move>();
-        for(Move m : moves){
-            game.movePiece(m.getStart().getRow(), m.getStart().getCol(), m.getDestination().getRow(), m.getDestination().getCol(), true);
-            values.add((double) game.evaluate());
-            game.reverseLastMove();
-        }
-        int size = values.size();
-        for(int i = 0; i < size; i++){
-            double val = Double.MAX_VALUE;
-            int idx = 0;
-            for(int j = 0; j < moves.size(); j++) {
-                if (values.get(j) < val) {
-                    val = values.get(j);
-                    idx = j;
-                }
-            }
-            result.add(moves.get(idx));
-            moves.remove(idx);
-            values.remove(idx);
-        }
-        if(game.getTurnCounter() % 2 == 0){
-            Collections.reverse(result);
-        }
-        moves = result;
     }
 
     public Move getBestMove(){
-        double value = state.turnCounter % 2 == 0 ? Double.MIN_VALUE : Double.MAX_VALUE;
-        int idx = 0;
-        if(state.turnCounter % 2 == 0){
-            for(Node child : root.getChildren()){
-                if (child.getValue() > value) {
-                    value = child.getValue();
-                    idx = root.getChildren().indexOf(child);
-                }
-            }
-        }
-        else{
-            for(Node child : root.getChildren()){
-                if (child.getValue() < value) {
-                    value = child.getValue();
-                    idx = root.getChildren().indexOf(child);
-                }
-            }
-        }
+        int idx = root.getChildren().indexOf(root.getbestNextNode());
         return possibleOutcomes.get(idx);
     }
 
--- core/src/com/group4/dicechess/agents/Expectimax/Node.java
+++ core/src/com/group4/dicechess/agents/Expectimax/Node.java
@@ -2,30 +2,31 @@ package com.group4.dicechess.agents.Expectimax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.group4.dicechess.GameState;
+import com.group4.dicechess.agents.NN_Evaluation.NN_Evaluation;
 
 public class Node{
     private Node parent;
-    private final List<Node> children;
+    private List<Node> children;
     private double value;
-    private final boolean chanceNode;
+    private Node bestNextNode;
+    private boolean chanceNode;
+    private int turnCount;
 
-
-    public Node(GameState state) {
+    public Node(GameState state){
         children = new ArrayList<Node>();
         value = 0;
         chanceNode = state.getDiceRoll() == 0;
+        turnCount = state.getTurnCounter();
         parent = null;
+        bestNextNode = null;
     }
-    
+
 
     public double getValue(){
         return value;
-    }
-
-    public void setValue(double value){
-        this.value = value;
     }
 
     public void setParent(Node parent){
@@ -48,5 +49,57 @@ public class Node{
         return children;
     }
 
+    public Node getbestNextNode(){
+        return bestNextNode;
+    }
+
+    public void computeValue(GameState state){
+        if(children.size() == 0){
+            value = state.evaluate();
+            //value = NN_Evaluation.evaluate(state.getBoard());
+        }
+        else{
+            if(chanceNode){
+                for(Node n : children){
+                    value += n.getValue();
+                }
+                value /= children.size();
+            }
+            else{
+                ArrayList<Node> equalNodes = new ArrayList<Node>();
+                equalNodes.add(children.get(0));
+                value = children.get(0).getValue();
+                if(turnCount % 2 == 0){
+                    for(Node n : children){
+                        if(n.getValue() > value){
+                            value = n.getValue();
+                            bestNextNode = n;
+                            equalNodes.clear();
+                            equalNodes.add(n);
+                        }
+                        else if(n.getValue() == value){
+                            equalNodes.add(n);
+                        }
+                    }
+
+                }
+                else{
+                    for(Node n : children){
+                        if(n.getValue() < value){
+                            value = n.getValue();
+                            bestNextNode = n;
+                            equalNodes.clear();
+                            equalNodes.add(n);
+                        }
+                        else if(n.getValue() == value){
+                            equalNodes.add(n);
+                        }
+                    }
+                }
+                Random rand = new Random();
+                bestNextNode = equalNodes.get(rand.nextInt(equalNodes.size()));
+            }
+        }
+    }
 
 }
